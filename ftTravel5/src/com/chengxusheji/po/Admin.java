b0f8daabd7b46119636f92cfd6cffcb7192b49f1
李昶旭 <lichangxu@bakejinfu.com>
2022-01-21 13:55:49 +0800

    实体类优化

--- ftTravel5/src/com/chengxusheji/po/Admin.java
+++ ftTravel5/src/com/chengxusheji/po/Admin.java
@@ -1,28 +1,16 @@
 package com.chengxusheji.po;
 
- 
+
+import lombok.Data;
 import org.hibernate.validator.constraints.NotEmpty;
 
-
+@Data
 public class Admin {
-	/*管理员用户名*/
-	@NotEmpty(message="用户名不能为空")
-	private String username;
-	/*登陆密码*/
-	@NotEmpty(message="登陆密码不能为空")
-	private String password;
-	
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
-	public void setPassword(String password) {
-		this.password = password;
-	}
-	
+    /*管理员用户名*/
+    @NotEmpty(message = "用户名不能为空")
+    private String username;
+    /*登陆密码*/
+    @NotEmpty(message = "登陆密码不能为空")
+    private String password;
+
 }
